ad60e54ba09f8bdbed3ae50e698dc5d49455ba3b
ZisisAvouris <zisisavouris@gmail.com>
2023-09-26 01:06:19 +0300

    delete old files

--- win_hexdump.c
+++
@@ -1,8 +0,0 @@
-#include "win_hexdump.h"
-
-int main(int argc, char *argv[])
-{
-    u32 test = 0xdeadbeef;
-    printf("%x\n", test);
-    return 0;
-}
--- win_hexdump.h
+++
@@ -1,7 +0,0 @@
-#ifndef WIN_HEXDUMP_H
-#define WIN_HEXDUMP_H
-
-#include <stdint.h>
-typedef uint32_t u32;
-
-#endif
